a5f0be38d40603f7a6495aade00e6f7d8b1da300
b.pirozhok <bogdan.pirozhok@itdecision-llc.com>
2021-07-26 01:47:17 +0300

    task-3 done

---
+++ task-4/task-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 6;
+            int middleLength = length / 2;
+            int[] range = new int[] { 1, 20 };
+            int[] array = new int[length];
+
+            Random R = new Random();
+
+            for (int i = 0; i < middleLength; i++)
+            {
+                array[i] = R.Next(range[0], range[1]);
+                array[i + middleLength] = array[i];
+            }
+
+            Console.WriteLine($"Результирующий массив: [{String.Join(", ", array)}]");
+        }
+    }
+}
