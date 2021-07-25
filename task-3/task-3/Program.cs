cb5dc924a5235525237da588a838d369bb2601c6
b.pirozhok <bogdan.pirozhok@itdecision-llc.com>
2021-07-26 01:36:09 +0300

    task-3 done

---
+++ task-3/task-3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 798;
+            int[] intList = value.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+
+            if (intList[0] > intList[1])
+            {
+                Console.WriteLine("Цифра сотни больше цифры десятка");
+            }
+
+            if (intList[0] > intList[2])
+            {
+                Console.WriteLine("Цифра сотни больше цифры еденицы");
+            }
+
+            if (intList[1] > intList[0])
+            {
+                Console.WriteLine("Цифра десятка больше цифры сотни");
+            }
+
+            if (intList[1] > intList[2])
+            {
+                Console.WriteLine("Цифра десятка больше цифры еденицы");
+            }
+
+            if (intList[2] > intList[0])
+            {
+                Console.WriteLine("Цифра еденицы больше цифры сотни");
+            }
+
+            if (intList[2] > intList[1])
+            {
+                Console.WriteLine("Цифра еденицы больше цифры десятка");
+            }
+        }
+    }
+}
